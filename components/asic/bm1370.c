2e9ee3ad5688fc28b7b3e77ec49a9d130dd1d1a2
edgecrusher <alex@platinumcs.ca>
2025-01-21 12:08:30 -0500

    Update bm1370.c

--- components/asic/bm1370.c
+++ components/asic/bm1370.c
@@ -399,7 +399,8 @@ void BM1370_set_job_difficulty_mask(int difficulty)
     // Correct:  {0b00000000, 0b00000000, 0b11111111, 0b11111111}
     // Incorrect: {0b00000000, 0b00000000, 0b11100111, 0b11111111}
     // (difficulty - 1) if it is a pow 2 then step down to second largest for more hashrate sampling
-    difficulty = _largest_power_of_two(difficulty) - 1;
+    //difficulty = _largest_power_of_two(difficulty) - 1;
+    difficulty = _largest_power_of_two(difficulty);
 
     // convert difficulty into char array
     // Ex: 256 = {0b00000000, 0b00000000, 0b00000000, 0b11111111}, {0x00, 0x00, 0x00, 0xff}
